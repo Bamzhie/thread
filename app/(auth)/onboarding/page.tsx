095663f27acece6301f0524cd514b541bef19b62
@bamzhie <sheuahmad@gmail.com>
2023-11-04 01:28:49 +0100

    updated communities logic

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -6,20 +6,23 @@ import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
-  if (!user) return null; // to avoid typescript warnings
 
-  const userInfo = await fetchUser(user.id);
-  if (userInfo?.onboarded) redirect("/");
-
-  const userData = {
-    id: user.id,
-    objectId: userInfo?._id,
-    username: userInfo ? userInfo?.username : user.username,
-    name: userInfo ? userInfo?.name : user.firstName ?? "",
-    bio: userInfo ? userInfo?.bio : "",
-    image: userInfo ? userInfo?.image : user.imageUrl,
+  const userInfo = {
+    id: "",
+    username: "",
+    name: "",
+    bio: "",
+    image: "",
   };
 
+  const userData = {
+    id: user?.id || "", // Provide a default value if user?.id is undefined
+    objectId: userInfo?.id,
+    username: userInfo?.username || user?.username || "",
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl || "",
+  };
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,7 +13,7 @@ interface Props {
     image: string;
     id: string;
   };
-  communityId: {
+  community: {
     id: string;
     name: string;
     image: string;
@@ -33,7 +34,7 @@ const ThreadCard = ({
   parentId,
   content,
   author,
-  communityId,
+  community,
   createdAt,
   comments,
   isComment,
@@ -100,6 +101,7 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
+
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
                   <p className="mt-1 text-subtle-medium text-gray-1">
@@ -110,7 +112,27 @@ const ThreadCard = ({
             </div>
           </div>
         </div>
+
+        {/* show comment logo */}
       </div>
+      {!isComment && community && (
+        <Link
+          href={`/communities/${community.id}`}
+          className="mt-5 flex items-center"
+        >
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createdAt)}
+            {community && ` - ${community.name} Community`}
+          </p>
+          <Image
+            src={community.image}
+            alt={community.name}
+            width={14}
+            height={14}
+            className="ml-1 rounded-full object-cover"
+          />
+        </Link>
+      )}
     </article>
   );
 };
